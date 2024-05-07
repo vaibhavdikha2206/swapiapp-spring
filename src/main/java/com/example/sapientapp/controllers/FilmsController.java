e57f0d29084ba1b25421014ff7ff61bf2af3c7ab
vaibhavdikha2206 <vaibhavdikha@gmail.com>
2024-05-08 03:38:45 +0530

    Added Test for Test

--- src/main/java/com/example/sapientapp/controllers/FilmsController.java
+++ src/main/java/com/example/sapientapp/controllers/FilmsController.java
@@ -26,7 +26,7 @@ public class FilmsController {
 
     @GetMapping("/test")
     public String getString() throws IOException {
-        return "Test22";
+        return "TestYo";
     }
 
     @GetMapping("/films/search")
